2198989d6e532db10a099d9e2aee705404d84eeb
Michael Orwin <mcokzoo@icloud.com>
2017-05-28 21:19:23 -0400

    File Upload Glitch
    - There is a huge glitch on file upload.  When downloading from github,
    selecting the local ‘iris.dat’ file IS NOT ALLOWED.  2 workarounds are
    clear.   (1) Paste the code into new files with the same name or (2)
    Make the dat file inputted by the user using <input type=“file>.  This
    second option is far more tricky.  No experience with FileReader etc.
    so just using the first method for rn.  Thus to view this project
    currently u must give yourself permissions or paste the code into new
    files.
    
    - The dropdown menus for the iris.dat were implemented and axes can
    change accordingly.  Maybe in the future the algorithms will be
    implemented in n-Dimensions but not currently
    
    Next Update: Labeling attributes on the canvas, adding a grid maybe,
    and labeling values
    
    Next (2) Update: Adding rating for each clustering algorithm by way of
    correctly guessed Iris species (time graph in canvas) (may be hard to
    implement?)

---
+++ Version 0.3/fuzzy.js
@@ -0,0 +1,195 @@
+// FUZZY
+var categories_FUZZY = []
+var centroids_FUZZY = []
+var nodesUnderCentroid_FUZZY = []
+var colors_FUZZY = []
+
+function makeRandomColors_FUZZY() {
+	var kValue = document.getElementById("kValue");
+	colors_FUZZY = ["#FFFF00","#00FFFF","#FF00FF","#FF0000","#00FF00","#0000FF"]
+	for (var i = 0; i<kValue.value-6; i++) {
+		var hex = "0123456789ABCDEF"
+		var color = "#";
+		for (var j = 0; j<6; j++) {
+			color+=hex[Math.floor(Math.random()*16)]
+		}
+		colors_FUZZY.push(color)
+	}
+}
+
+function randomizeCentroidLocations_FUZZY() {
+	//centroids_FUZZY = []
+	var kValue = document.getElementById("kValue");
+	for (var i = 0; i<kValue.value; i++) {
+		centroids_FUZZY.push([Math.random(),Math.random()])
+	}
+}
+
+function drawCentroids_FUZZY() {
+	var canvas = document.getElementById("canvas2");
+	var ctx = canvas.getContext('2d');
+	for (var i = 0; i<centroids_FUZZY.length; i++) {
+		var centroid = centroids_FUZZY[i]
+		ctx.fillStyle = "#000000";
+		ctx.strokeStyle = colors_FUZZY[i];
+		//ctx.fillStyle = "#AACCFF";
+		//ctx.strokeStyle = "#3377BB";
+		//ctx.fillStyle = "#FFAAAA";
+		//ctx.strokeStyle = "#FF5555";
+		ctx.lineWidth = 4;
+		// x y reversed
+		ctx.beginPath();
+		ctx.arc(centroid[0]*canvas.height, canvas.width-centroid[1]*canvas.width,25,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		//ctx.fillRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+		//ctx.strokeRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+	}
+}
+
+function assignCategory_FUZZY() {
+	var xaxis = document.getElementById("xaxis")
+	var yaxis = document.getElementById("yaxis")
+
+	var fuzzyValue = document.getElementById("fuzzyValue")
+	if (categories_FUZZY.length == 0) {
+		categories_FUZZY = new Array(data.length).fill([]);
+		for (var i = 0; i<categories_FUZZY.length; i++) {
+			categories_FUZZY[i] = new Array(centroids_FUZZY.length).fill(-1)
+		}
+	}
+	//console.log("before")
+	//console.log(categories_FUZZY)
+	//console.log(categories_FUZZY)
+	//console.log("inside assign")
+	//console.log(centroids_KMEANS)
+	for (var i = 0; i<data.length; i++) {
+		var datapoint = data[i];
+		var sum = 0;
+		for (var j = 0; j<centroids_FUZZY.length; j++) {
+			//console.log(i+" "+j)
+			var centroid = centroids_FUZZY[j]
+			var dist = Math.pow((datapoint[xaxis.value]-centroid[0]),2)+Math.pow((datapoint[yaxis.value]-centroid[1]),2)
+			sum+=dist
+			//console.log(dist)
+			categories_FUZZY[i][j] = dist
+		}
+		var newSum = 0;
+		for (var j = 0; j<centroids_FUZZY.length; j++) {
+			categories_FUZZY[i][j] = Math.pow(sum-categories_FUZZY[i][j],fuzzyValue.value)
+			newSum+=categories_FUZZY[i][j]
+		}
+		for (var j = 0; j<centroids_FUZZY.length; j++) {
+			categories_FUZZY[i][j] /= newSum
+		}
+	}
+	//console.log(categories_FUZZY)
+	//console.log("after")
+	//console.log(categories_FUZZY)
+}
+
+function computeColor(indexOfData) {
+	var r = 0;
+	var g = 0;
+	var b = 0;
+
+	for (var i = 0; i<centroids_FUZZY.length; i++) {
+		var currentColor = colors_FUZZY[i]
+		//console.log(colors_FUZZY[i])
+		//console.log(categories_FUZZY)
+		r += categories_FUZZY[indexOfData][i]*hexToDec(currentColor.substring(1,3))
+		g += categories_FUZZY[indexOfData][i]*hexToDec(currentColor.substring(3,5))
+		b += categories_FUZZY[indexOfData][i]*hexToDec(currentColor.substring(5,7))
+
+	}
+	var hexR = decToHex(Math.floor(r))
+	var hexG = decToHex(Math.floor(g))
+	var hexB = decToHex(Math.floor(b))
+
+	return "#"+hexR+hexG+hexB
+}
+
+function hexToDec(hex) {
+	return parseInt(hex,16);
+}
+
+function decToHex(dec) {
+	var hex = dec.toString(16)
+	if (hex.length < 2) {
+		hex = "0"+hex
+	}
+	return hex
+}
+
+function replotData_FUZZY() {
+	var xaxis = document.getElementById("xaxis")
+	var yaxis = document.getElementById("yaxis")
+
+	//console.log(categories_FUZZY)
+	clearCanvas("canvas2")
+	var canvas = document.getElementById("canvas2");
+	var ctx = canvas.getContext('2d');
+	//console.log(colors_FUZZY)
+	//console.log(centroids_FUZZY)
+	for (var i = 0; i<data.length; i++) {
+		var datapoint = data[i]
+		ctx.fillStyle = computeColor(i);
+		//console.log(computeColor(i))
+		ctx.strokeStyle = "#000000";
+		//ctx.fillStyle = "#AACCFF";
+		//ctx.strokeStyle = "#3377BB";
+		//ctx.fillStyle = "#FFAAAA";
+		//ctx.strokeStyle = "#FF5555";
+		ctx.lineWidth = 2;
+		// x y reversed
+		ctx.beginPath();
+		ctx.arc(datapoint[xaxis.value]*canvas.height, canvas.width-datapoint[yaxis.value]*canvas.width,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		//ctx.fillRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+		//ctx.strokeRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+	}
+}
+
+function clearCentroidData_FUZZY() {
+	centroids_FUZZY = []
+	nodesUnderCentroid_FUZZY = []
+	//previousCentroids = []
+	for (var i = 0; i<kValue.value; i++) {
+		centroids_FUZZY.push([0,0])
+		nodesUnderCentroid_FUZZY.push(0)
+		//previousCentroids.push([])
+	}
+}
+
+function calcCentroids_FUZZY() {
+	var xaxis = document.getElementById("xaxis")
+	var yaxis = document.getElementById("yaxis")
+
+	clearCentroidData_FUZZY()
+
+	for (var i = 0; i<data.length; i++) {
+		var datapoint = data[i];
+		for (var j = 0; j<centroids_FUZZY.length; j++) {
+			nodesUnderCentroid_FUZZY[j] += categories_FUZZY[i][j]
+			centroids_FUZZY[j][0]+=datapoint[xaxis.value]*categories_FUZZY[i][j]
+			centroids_FUZZY[j][1]+=datapoint[yaxis.value]*categories_FUZZY[i][j]
+		}
+	}
+	for (var i = 0; i<centroids_FUZZY.length; i++) {
+		if (nodesUnderCentroid_FUZZY[i]==0) {
+			centroids_FUZZY[i][0] = Math.random()
+			centroids_FUZZY[i][1] = Math.random()
+			nodesUnderCentroid_FUZZY[i]++;
+		}
+		centroids_FUZZY[i][0]/=nodesUnderCentroid_FUZZY[i]
+		centroids_FUZZY[i][1]/=nodesUnderCentroid_FUZZY[i]
+	}
+}
+
+function reset_FUZZY() {
+	categories_FUZZY = []
+	centroids_FUZZY = []
+	nodesUnderCentroid_FUZZY = []
+	colors_FUZZY = []
+}
---
+++ Version 0.3/kmeans.js
@@ -0,0 +1,134 @@
+// KMEANS
+var categories_KMEANS = []
+var centroids_KMEANS = []
+var nodesUnderCentroid_KMEANS = []
+var colors_KMEANS = []
+
+function makeRandomColors_KMEANS() {
+	var kValue = document.getElementById("kValue");
+	colors_KMEANS = ["#FF0000","#00FF00","#0000FF","#FFFF00","#00FFFF","#FF00FF"]
+	for (var i = 0; i<kValue.value-6; i++) {
+		var hex = "0123456789ABCDEF"
+		var color = "#";
+		for (var j = 0; j<6; j++) {
+			color+=hex[Math.floor(Math.random()*16)]
+		}
+		colors_KMEANS.push(color)
+	}
+}
+
+
+function randomizeCentroidLocations_KMEANS() {
+	var kValue = document.getElementById("kValue");
+	for (var i = 0; i<kValue.value; i++) {
+		centroids_KMEANS.push([Math.random(),Math.random()])
+	}
+}
+function drawCentroids_KMEANS() {
+	var canvas = document.getElementById("canvas0");
+	var ctx = canvas.getContext('2d');
+	for (var i = 0; i<centroids_KMEANS.length; i++) {
+		var centroid = centroids_KMEANS[i]
+		ctx.fillStyle = "#000000";
+		ctx.strokeStyle = colors_KMEANS[i];
+		//ctx.fillStyle = "#AACCFF";
+		//ctx.strokeStyle = "#3377BB";
+		//ctx.fillStyle = "#FFAAAA";
+		//ctx.strokeStyle = "#FF5555";
+		ctx.lineWidth = 4;
+		// x y reversed
+		ctx.beginPath();
+		ctx.arc(centroid[0]*canvas.height, canvas.width-centroid[1]*canvas.width,25,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		//ctx.fillRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+		//ctx.strokeRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+	}
+}
+
+function assignCategory_KMEANS() {
+	var xaxis = document.getElementById("xaxis")
+	var yaxis = document.getElementById("yaxis")
+
+	if (categories_KMEANS.length == 0) {
+		categories_KMEANS = new Array(data.length).fill(-1);
+	}
+	//console.log("inside assign")
+	//console.log(centroids_KMEANS)
+	for (var i = 0; i<data.length; i++) {
+		var minDist = Number.MAX_VALUE;
+		var minCentroid = -1;
+		var datapoint = data[i];
+
+		for (var j = 0; j<centroids_KMEANS.length; j++) {
+			var centroid = centroids_KMEANS[j]
+			var dist = Math.pow((datapoint[xaxis.value]-centroid[0]),2)+Math.pow((datapoint[yaxis.value]-centroid[1]),2)
+			if (dist < minDist) {
+				minDist = dist;
+				minCentroid = j;
+			}
+		}
+		//console.log(minCentroid)
+		categories_KMEANS[i] = minCentroid
+	}
+}
+
+function replotData_KMEANS() {
+	//var xaxis = document.getElementById("xaxis")
+	//var yaxis = document.getElementById("yaxis")
+
+	clearCanvas("canvas0")
+	var canvas = document.getElementById("canvas0");
+	var ctx = canvas.getContext('2d');
+
+	for (var i = 0; i<data.length; i++) {
+		var datapoint = data[i]
+		ctx.fillStyle = colors_KMEANS[categories_KMEANS[i]];
+		ctx.strokeStyle = "#000000";
+		//ctx.fillStyle = "#AACCFF";
+		//ctx.strokeStyle = "#3377BB";
+		//ctx.fillStyle = "#FFAAAA";
+		//ctx.strokeStyle = "#FF5555";
+		ctx.lineWidth = 2;
+		// x y reversed
+		ctx.beginPath();
+		ctx.arc(datapoint[xaxis.value]*canvas.height, canvas.width-datapoint[yaxis.value]*canvas.width,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		//ctx.fillRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+		//ctx.strokeRect(scaledLoc.x,scaledLoc.y,pixelSize,pixelSize);
+	}
+}
+function clearCentroidData_KMEANS() {
+	centroids_KMEANS = []
+	nodesUnderCentroid_KMEANS = []
+	//previousCentroids = []
+	for (var i = 0; i<kValue.value; i++) {
+		centroids_KMEANS.push([0,0])
+		nodesUnderCentroid_KMEANS.push(0)
+		//previousCentroids.push([])
+	}
+}
+
+function calcCentroids_KMEANS() {
+	//var xaxis = document.getElementById("xaxis")
+	//var yaxis = document.getElementById("yaxis")
+	clearCentroidData_KMEANS()
+
+	for (var i = 0; i<data.length; i++) {
+		datapoint = data[i];
+		category = categories_KMEANS[i]
+		nodesUnderCentroid_KMEANS[category]++
+		centroids_KMEANS[category][0]+=datapoint[xaxis.value]
+		centroids_KMEANS[category][1]+=datapoint[yaxis.value]
+	}
+	for (var i = 0; i<centroids_KMEANS.length; i++) {
+		if (nodesUnderCentroid_KMEANS[i]==0) {
+			centroids_KMEANS[i][0] = Math.random()
+			centroids_KMEANS[i][1] = Math.random()
+			nodesUnderCentroid_KMEANS[i]++;
+		}
+		centroids_KMEANS[i][0]/=nodesUnderCentroid_KMEANS[i]
+		centroids_KMEANS[i][1]/=nodesUnderCentroid_KMEANS[i]
+	}
+}
